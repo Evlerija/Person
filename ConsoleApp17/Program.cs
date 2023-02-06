d5af9245d9f22ffa3f2ef47d52fa2bdb429ced3d
Kul33 <Kul33@DESKTOP-J8QGFCQ>
2023-02-06 07:27:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    public partial class Program : Person
+    {
+        static void Main(string[] args)
+        {
+            Person Alex = new Person("Alex", 24);
+            Person Alexander = new Person();
+            Console.WriteLine(Alex.Move(Alex.Age));
+            Console.WriteLine(Alex.Talk(Alex.FullName));
+        }
+        
+        
+    }
+    public class Person
+    {
+        public string FullName; public sbyte Age;
+        public Person(string fullname, sbyte age) 
+        {
+            FullName = fullname;
+            Age = age;
+        }
+        public string Talk(string fullname)
+        {
+            string FullName = "Такой- то "  + fullname + " говорит.";
+            return FullName;
+        }
+        public string Move(sbyte age)
+        {
+            string Age = "Возраст " + age;
+            return Age;
+        }
+        public Person()
+        {
+            return;
+        }
+    }
+}
